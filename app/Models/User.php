2c1f5e2f232ab15bbf0f1757b13ab9d24345726a
jhontabo <jhonse.tajumbina@gmail.com>
2024-11-20 23:04:11 -0500

    add contract filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, 'jhonse.tajumbina@umariana.edu.co') && $this->hasVerifiedEmail();
     }
 
     protected $fillable = [
